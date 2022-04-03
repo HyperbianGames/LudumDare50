8a4947073a2893da18361fd04419aef407a19669
zinnin <34364024+zinnin@users.noreply.github.com>
2022-04-03 10:51:25 -0700

    REORG

---
+++ Assets/Scripts/GameMenuController.cs
@@ -0,0 +1,83 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameMenuController : MonoBehaviour
+{
+    public static GameMenuController Instance;
+
+    public GameObject Menu;
+    public GameObject WinScreen;
+    public GameObject LoseScreen;
+    public GameObject ElevatorSelect;
+    public GameObject WarningTextObj;
+
+    private void Start()
+    {
+        Instance = this;
+    }
+
+    public void ToggleMenu()
+    {
+        Menu.SetActive(!Menu.activeInHierarchy);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Reset()
+    {
+        Time.timeScale = 1;
+       SceneManager.LoadScene(2);
+    }
+
+    public void GoToMenu()
+    {
+        ShowMenu();
+    }
+
+    public void ShowWinScreen()
+    {
+        if (WinScreen != null)
+            WinScreen.SetActive(true);
+    }
+
+    public void ShowLoseScreen()
+    {
+        if (LoseScreen != null)
+            LoseScreen.SetActive(true);
+    }
+
+    internal void ShowMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(1);
+    }
+
+    public void ShowElevator(int numberOfTowersActive)
+    {
+        WarningTextObj.GetComponent<Text>().text = $"There are still {numberOfTowersActive} Pillars remaining. Defeat the enemies near the pillars to destroy them. Destroying Pillars will weaken Boss Name Here.\n\nAre you sure you want to go fight Boss Name Here ? ";
+
+        ElevatorSelect.SetActive(true);
+    }
+
+    public void HideElevator()
+    {
+        ElevatorSelect.SetActive(false);
+    }
+
+    public void ElavtorYesClick()
+    {
+        ElavatorController.Instance.StartRaise();
+        HideElevator();
+    }
+
+    public void ElavtorNoClick()
+    {
+        HideElevator();
+        PlayerController.Instance.ReleasePlayer();
+    }
+}
