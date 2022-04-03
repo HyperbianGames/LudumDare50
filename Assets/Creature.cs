a89fc0e8902f7eb2504a8d8ee5eb005cd9ca406c
zinnin <34364024+zinnin@users.noreply.github.com>
2022-04-02 17:24:37 -0700

    You can win

--- Assets/Creature.cs
+++ Assets/Creature.cs
@@ -17,6 +17,8 @@ public class Creature : MonoBehaviour
 
     public Dictionary<Spell, float> Cooldowns { get; set; } = new Dictionary<Spell, float>();
     public bool IsPlayer { get; set; } = false;
+    public bool IsKillable = true;
+    public bool IsBoss = false;
     public Creature CurrentTarget { get; set; } = null;
 
     public NavMeshAgent agent;
@@ -31,7 +33,7 @@ public class Creature : MonoBehaviour
 
     private void OnDestroy()
     {
-        CombatManager.Instance.Creatures.Remove(this);
+        CombatManager.Instance.CreatureDestroyed(this);
     }
 
     // Update is called once per frame
@@ -70,7 +72,14 @@ public class Creature : MonoBehaviour
         }
         else
         {
-            CurrentHealth = MaxHealth;
+            if (IsKillable)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                CurrentHealth = MaxHealth;
+            }
         }
         
         if (HealthBar != null)
--- Assets/MenuController.cs
+++ Assets/MenuController.cs
@@ -1,11 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
-{
+{    
     private void Start()
     {
         SceneManager.sceneLoaded += SceneManager_sceneLoaded;
